41eb53a959bd786d66d1eceb945d9a60d29bf890
Artem Oleksandrenko <artem2008.zp@gmail.com>
2024-08-21 10:46:25 +0300

    Update JS code

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ questions.forEach((question) => {
     question.addEventListener('click', () => {
         const answerCurrent = question.nextElementSibling;
         const plus = question.lastChild;
+        const isOpen = answerCurrent.classList.contains('open');
         answers.forEach((answer) => {
             answer.classList.remove('open');
             answer.style.maxHeight = 0 + 'px';
@@ -12,8 +13,10 @@ questions.forEach((question) => {
         pluses.forEach((pl) => {
             pl.textContent = '+';
         });
-        plus.textContent = '-';
+        if (isOpen === false) {
+            plus.textContent = '-';
         answerCurrent.classList.add('open');
         answerCurrent.style.maxHeight = answerCurrent.scrollHeight + 'px';
+        }
     });
 });
